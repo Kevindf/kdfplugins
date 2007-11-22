d923b387f0f3d211ed7d9bcc2c24696291758bb0
kevindf <kevindf@deb61619-3e3f-0410-b7c9-19872b24e330>
2007-11-22 07:12:43 +0000

    tag a 6.3.x version of the Scanner

--- Scanner.pm
+++ Scanner.pm
@@ -1,50 +1,47 @@
-package Plugins::Scanner::Plugin;
+package Plugins::Scanner;
 
-# $Id: Plugin.pm,v 1.2 2007-11-10 04:36:58 fishbone Exp $
+# $Id: Scanner.pm,v 1.5 2006-04-29 09:05:53 fishbone Exp $
 # by Kevin Deane-Freeman August 2004
 
 # This code is derived from code with the following copyright message:
 #
-# SqueezeCenter Copyright (c) 2001-2007 Logitech
+# SliMP3 Server Copyright (C) 2001 Sean Adams, Slim Devices Inc.
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License,
 # version 2.
-
-# To use this as a single-button access, add the following to a custom.map file:
-# fwd.hold =  menu_Plugins::Scanner::Plugin
-#
 use strict;
-
-use Slim::Utils::Prefs;
-use base qw(Slim::Plugin::Base);
 
 ###########################################
 ### Section 1. Change these as required ###
 ###########################################
 
+use Slim::Control::Command;
 use Slim::Utils::Strings qw (string);
 use File::Spec::Functions qw(:ALL);
 
 use vars qw($VERSION);
-$VERSION = substr(q$Revision: 1.2 $,10);
+$VERSION = substr(q$Revision: 1.5 $,10);
 
 my $offset=0;
 
 sub getDisplayName {return 'PLUGIN_SCANNER'}
 
+sub strings { return '
+PLUGIN_SCANNER
+	EN	Song Scanner
+
+PLUGIN_SCANNER_SET
+	EN	Scan to:
+
+PLUGIN_SCANNER_PLAY
+	EN	 (press PLAY)
+'};
+
 ##################################################
 ### Section 2. Your variables and code go here ###
 ##################################################
 
-my $log          = Slim::Utils::Log->addLogCategory({
-	'category'     => 'plugin.songscanner',
-	'defaultLevel' => 'WARN',
-	'description'  => getDisplayName(),
-});
-
-
 my %current;
-my $jumptomode;
 
 my %menuParams = (
 	'scanner' => {
@@ -63,8 +60,8 @@ my %menuParams = (
 				,'max' => undef
 				,'increment' => undef
 				,'onChange' => sub { $offset = $_[1];
-								$_[0]->modeParam('max',Slim::Player::Source::playingSongDuration($_[0]));
-								$_[0]->modeParam('increment',Slim::Player::Source::playingSongDuration($_[0])/100);
+								$_[0]->param('max',Slim::Player::Source::playingSongDuration($_[0]));
+								$_[0]->param('increment',Slim::Player::Source::playingSongDuration($_[0])/100);
 							}
 				,'onChangeArgs' => 'CV'
 				,'callback' => \&scannerExitHandler
@@ -75,11 +72,7 @@ my %menuParams = (
 sub scannerExitHandler {
 	my ($client,$exittype) = @_;
 	$exittype = uc($exittype);
-	
-	if ($jumptomode) {
-		Slim::Buttons::Common::popMode($client);
-		$jumptomode = 0;
-	} elsif ($exittype eq 'LEFT') {
+	if ($exittype eq 'LEFT') {
 		Slim::Buttons::Common::popModeRight($client);
 		#Slim::Player::Source::gototime($client, $offset, 1);
 	} elsif ($exittype eq 'RIGHT') {
@@ -93,26 +86,17 @@ my %functions = (
 	'right' => sub  {
 		my ($client,$funct,$functarg) = @_;
 		scannerExitHandler($client,'RIGHT');
-	},
-	'left' => sub {
+	}
+	,'left' => sub {
 		my $client = shift;
 		scannerExitHandler($client,'LEFT');
-	},
-	'play' => sub {
+	}
+	,'play' => sub {
 		my $client = shift;
 		Slim::Player::Source::playmode($client,"play");
 		Slim::Player::Source::gototime($client, $offset, 1);
 		$client->showBriefly($client->currentSongLines());
-		if ($jumptomode) {
-			Slim::Buttons::Common::popMode($client);
-			$jumptomode = 0;
-		}
-	},
-	'jumptoscanner' => sub {
-		my $client = shift;
-		Slim::Buttons::Common::pushModeLeft( $client, 'Plugins::Scanner::Plugin');
-		$jumptomode = 1;
-	},
+	}
 );
 
 sub getFunctions {
@@ -122,12 +106,10 @@ sub getFunctions {
 sub lines {
 	my $line1 = string('PLUGIN_SCANNER');
 	my $line2 = '';
-	
-	return {'line'    => [$line1, $line2],}
+	return ($line1,$line2);
 }
 
 sub setMode {
-	my $class  = shift;
 	my $client = shift;
 	my $method = shift;
 	if ($method eq 'pop') {
@@ -141,9 +123,4 @@ sub setMode {
 	$offset = Slim::Player::Source::songTime($client);
 	Slim::Buttons::Common::pushMode($client,'INPUT.Bar',\%params);
 	$client->update();
-}
-
-sub initPlugin {
-	my $class = shift;
-	$class->SUPER::initPlugin();
 }
